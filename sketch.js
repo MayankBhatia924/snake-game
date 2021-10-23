67954b16e2af708fe8c9fe9f60f83c0ada01e277
MayankBhatia924 <85504542+MayankBhatia924@users.noreply.github.com>
2021-10-23 18:38:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(15);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+}
+
+function draw() {
+  scale(rez);
+  background("gray");
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+  if (snake.endGame()) {
+    print('END GAME');
+    background("red");
+    noLoop();
+    
+  }
+
+  noStroke();
+  fill("red");
+  rect(food.x, food.y, 1, 1);
+}
